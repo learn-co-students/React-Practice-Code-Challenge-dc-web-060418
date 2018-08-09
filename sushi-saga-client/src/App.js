29f01d24fabe59be377e25234712134b966d8fe9
jchamberlain909 <jchamberlain909@gmail.com>
2018-08-09 14:36:10 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,58 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  constructor(){
+    super()
+    this.allSushi = []
+    this.state={
+      displayedSushi: [],
+      customerMoney: 100,
+      numSushiEaten: 0
+    }
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(res=>res.json())
+    .then(sushiData=>{
+      this.allSushi = sushiData
+      this.setState({
+        displayedSushi: this.newSushi()
+      })
+    })
+  }
+
+  moreSushi = ()=>{
+    this.setState({
+      displayedSushi: this.newSushi()
+    })
+  }
+
+  newSushi = () => {
+    let returnSushi = []
+    for (let i = 0; i < 4; i++) {
+      let randIndex = Math.floor(Math.random() * this.allSushi.length)
+      returnSushi.push(this.allSushi[randIndex])
+    }
+    return returnSushi
+  }
+
+  eatSushi= (price) => {
+    this.setState({
+      customerMoney: this.state.customerMoney - price,
+      numSushiEaten: this.state.numSushiEaten + 1
+    })
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer   displayedSushi={this.state.displayedSushi}
+                          moreSushi={this.moreSushi}
+                          eatSushi={this.eatSushi}
+                          customerMoney={this.state.customerMoney}/>
+        <Table            customerMoney={this.state.customerMoney}
+                          numSushiEaten={this.state.numSushiEaten} />
       </div>
     );
   }
