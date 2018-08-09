ebf514244627f9beda2085d2071c9bd1fc3d7534
github username <github email address>
2018-08-09 18:10:52 -0400

    Finished non-bonus deliverables

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,17 +1,49 @@
 import React, { Component } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
-
-// Endpoint!
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      sushiList: [],
+      sliceX: 0,
+      sliceY: 4,
+      eatenSushi: [],
+      wallet: 100
+    }
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(resp => resp.json())
+      .then(json => {
+        this.setState({sushiList: json})
+      })
+  }
+
+  getMoreSushi = () => {
+    this.setState({sliceX: (this.state.sliceX + 4), sliceY: (this.state.sliceY + 4)})
+  }
+
+  addToEatenSushiList = (sushi) => {
+    if (!this.state.eatenSushi.includes(sushi)){
+      this.setState({eatenSushi: [...this.state.eatenSushi, sushi], wallet: (this.state.wallet - sushi.price)})
+    }
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          sushiList={this.state.sushiList.slice(this.state.sliceX, this.state.sliceY)}
+          moreButton={this.getMoreSushi}
+          ateSushi={this.addToEatenSushiList}
+          currentWallet={this.state.wallet}
+        />
+      <Table eatenSushi={this.state.eatenSushi} wallet={this.state.wallet}/>
       </div>
     );
   }
